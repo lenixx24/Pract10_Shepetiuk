518a6d6828db207c8ba2abb4ce5284569653179c
Angelina Shepetiuk <a.shepetiuk@ukma.edu.ua>
2026-03-24 12:32:25 +0200

    feat: add sort with Lambda, Anon class and Method ref

---
+++ src/main/java/ua/university/LambdaRefactorLab.java
@@ -0,0 +1,36 @@
+package ua.university;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaRefactorLab {
+    public static void sortAnonymous(List<String> strings){
+        strings.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+    }
+    public static void sortLambda(List<String> strings){
+        strings.sort((a, b) -> a.compareTo(b));
+    }
+    public static void sortMethodRef(List<String> strings){
+        strings.sort(String::compareTo);
+    }
+
+    static void main() {
+        List<String> strings = new ArrayList<>();
+        strings.add("B"); strings.add("c"); strings.add("A"); strings.add("anonymous"); strings.add("y");
+        System.out.println(strings);
+        sortAnonymous(strings);
+        System.out.println("Anonymous: "+strings);
+        strings.add("lambda");
+        sortLambda(strings);
+        System.out.println("Lambda: "+strings);
+        strings.add("methodref");
+        sortMethodRef(strings);
+        System.out.println("Method ref: "+strings);
+    }
+}
